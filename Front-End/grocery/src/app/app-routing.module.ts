06f3ba6bb497cfd3834b13078796d4791743ddfc
Amer mohamed <87991363+AmerM1@users.noreply.github.com>
2021-09-14 13:58:14 -0400

    added user and employee routes

--- Front-End/grocery/src/app/app-routing.module.ts
+++ Front-End/grocery/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component';
 const routes: Routes = [
 
   {path:"",component:HomepageComponent},
   {path:"adminlogin",component:AdminComponent},
-  {path:"employeelogin",component:EmployeeComponent}
+  {path:"employeelogin",component:EmployeeComponent},
+  {path:"userlogin",component:UserComponent}
 ];
 
 @NgModule({
