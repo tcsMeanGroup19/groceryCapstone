f9cf5d2638ad59fb2a166a850ba12406bfe2324d
Amer mohamed <87991363+AmerM1@users.noreply.github.com>
2021-09-15 17:30:30 -0400

    raise ticket

--- Back-End/app.js
+++ Back-End/app.js
@@ -2,8 +2,7 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let cors = require("cors");
-let routerProduct = require("./router/product.router");
-let userRouter = require("./router/user.router");
+let requestRouter = require("./Routers/request.router");
 
 // create the reference of express 
 let app = express();
@@ -23,6 +22,6 @@ mongoose.connect(url).then(res=>console.log("connected")).catch(error=>console.l
 // app.use("/api/product",routerProduct);
 
 
-// app.use("/api/user",userRouter);
+app.use("/api/requests",requestRouter);
 
 app.listen(9090,()=>console.log("Server running on port number 9090"))
---
+++ Front-End/grocery/src/app/raiseticket.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { RaiseTicket } from './raiseticket';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaiseticketService {
+
+  constructor(public http:HttpClient) { }
+
+  raiseTicket(RaiseTicket:RaiseTicket):Observable<any>{
+    return this.http.post("http://localhost:9090/api/requests",RaiseTicket,
+    {responseType:'text'});
+  }
+}
