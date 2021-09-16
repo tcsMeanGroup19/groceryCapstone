1ff9146f58a7bac3f079b58c952dfe5507280a17
Ryan Dunn <47995602+RyDunn97@users.noreply.github.com>
2021-09-16 09:49:22 -0700

    adding buttons

--- Front-End/grocery/src/app/userpanel/userpanel.component.ts
+++ Front-End/grocery/src/app/userpanel/userpanel.component.ts
@@ -7,13 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserpanelComponent implements OnInit {
 
+  msg:string="";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // show history of orders and their status 
   checkOrderStatus(){
+    this.msg = "hello";
 
+  }
+
+  // edit password, address, phone num, email id
+  editProfile(){
+    this.msg = "editing profile";
+  }
+
+  // show the funds currently available
+  showFunds(){
+    this.msg = "showing funds";
+  }
+
+  // add funds to the account
+  addFunds(){
+    this.msg = "adding funds";
+
+    // does it call showFunds() at the end?
   }
   
 }
